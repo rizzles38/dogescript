714541a69df5e927a17ff9c7206be3f5aef438cc
mr1337357 <mr1337357@gmail.com>
2013-11-11 15:31:03 -0800

    writing main

--- code interpret/very java/src/Interpret.java
+++ code interpret/very java/src/Interpret.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class Interpret {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		if(args.length<1) {
+		  System.out.printf("wow. no file.\n");
+		  System.exit(-1);
+		}
+		File f = new File(args[0]);
+		Scanner sc = new Scanner("");
+		try {
+      sc = new Scanner(f);
+    } catch (FileNotFoundException e) {
+      System.out.print("wow. bad file.\n");
+      System.exit(-1);
+    }
+		System.out.printf("wow %s\n", sc.next());
 	}
 
 }
